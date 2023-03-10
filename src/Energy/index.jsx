81665e4e708b6dab618d6c05472ebf42beffda16
Monika <dedvseved@seznam.cz>
2023-03-11 00:47:15 +0100

    vypracovani domaciho ukolu

---
+++ src/Energy/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './style.css';
+import '../style.css';
+import Electricity from './images/electricity.svg';
+import Water from './images/water.svg';
+
+const Energy = ({ electricity, water }) => {
+    return (
+        <div className="energy">
+            <div className="energy__source">
+                <div className="energy__icon">
+                    <img src={Electricity} />
+                </div>
+                <div className="energy__consumption">
+                    <div className="energy__description">Elektřina</div>
+                    <div className="energy__value">{electricity} kW</div>
+                </div>
+            </div>
+            <div className="energy__source">
+                <div className="energy__icon">
+                    <img src={Water} />
+                </div>
+                <div className="energy__consumption">
+                    <div className="energy__description">Voda</div>
+                    <div className="energy__value">{water} m<sup>3</sup></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Energy;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import Header from './Header';
+import Dashboard from './Dashboard';
+import smartHomeData from './smartHomeData.js';
 
 const App = () => (
-  <>
-
-  </>
+  <div className="container">
+    <Header title={"Chytrý dům"} />
+    <Dashboard data={smartHomeData} />
+	</div>
 );
 
 createRoot(
